6be42d5400247f45e4a5939e631eb4f52b0ac340
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2022-05-31 14:05:20 +0200

    Set up interface animation and quadtree visualization

--- webgui/scripts/animation.js
+++ webgui/scripts/animation.js
@@ -1,75 +1,157 @@
 console.log("loaded animation.js");
 
-async function readFile(node) {
-    const file = node.files[0];
-    const obj = await fileToJSON(file);
-    console.log(obj);
-    simulateParticlesAnimation();
+async function getBBoxes(data) {
+    return await Promise.all(data.map(async (o) => {
+        return new Promise((resolve, reject) => {
+            resolve({
+                tl: o.quadtree.top_left,
+                br: [o.quadtree.top_left[0] + o.quadtree['length'], o.quadtree.top_left[1] - o.quadtree['length']]
+            })
+        })
+    }));
+
 }
 
-function generateCircles() {
-    for (let i = 0; i < N_STEPS; i++) {
-        let circles = [];
-        for (let j = 0; j < N_CIRCLES; j++) {
-            // if it's the first step in history
-            if (i === 0) {
-                let cx = Math.floor(Math.random() * SVG_SIZE);
-                let cy = Math.floor(Math.random() * SVG_SIZE);
-                circles.push({idx: j, id: "particle_" + j, cx, cy});
-            } else {
-                let cx = circlesHistory[i - 1][j].cx + (Math.floor(Math.random() * 100) - 50);
-                let cy = circlesHistory[i - 1][j].cy + (Math.floor(Math.random() * 100) - 50);
-                circles.push({idx: j, id: "particle_" + j, cx, cy});
-            }
-        }
-        circlesHistory.push(circles);
+function getMaxBBox(bBoxes) {
+    let maxBBox = {
+        br: [-Infinity, Infinity],
+        tl: [Infinity, -Infinity]
     }
-    console.log(circlesHistory);
+
+    bBoxes.forEach(function (bBox) {
+        if (bBox.tl[0] < maxBBox.tl[0]) maxBBox.tl[0] = bBox.tl[0];
+        if (bBox.tl[1] > maxBBox.tl[1]) maxBBox.tl[1] = bBox.tl[1];
+        if (bBox.br[0] > maxBBox.br[0]) maxBBox.br[0] = bBox.br[0];
+        if (bBox.br[1] < maxBBox.br[1]) maxBBox.br[1] = bBox.br[1];
+    })
+
+    return maxBBox
 }
 
-// I take the first step in history to set up the cirlces on screen
-function appendCircles() {
-    circlesHistory[0].map((circle) => {
-        appendCircle(circle.cx, circle.cy, circle.id);
-    });
+async function getParticlesSteps(data) {
+    let particlesStep = await Promise.all(data.map(o => {
+        return new Promise(async (resolve, reject) => {
+            let p = await Promise.all(o.bodies.map(particle => {
+                {
+                    return new Promise((resolve1, reject1) => {
+                        resolve1({
+                            mass: particle.mass,
+                            position: particle.position
+                        })
+                    })
+                }
+            }))
+            resolve(p)
+        })
+    }))
+    return particlesStep
+
+}
+
+async function getSvgNormalizedHistory(maxBBox, particlesStep) {
+    const SVG_SIZE = parseInt(document.getElementById("svg-canvas").getAttribute("width"));
+    const SCENARIO_SIZE = maxBBox.br[0] - maxBBox.tl[0]
+    let ratio = SVG_SIZE / SCENARIO_SIZE;
+    console.log("scenario size: " + SCENARIO_SIZE + " | svg size: " + SVG_SIZE + " | ratio: " + ratio)
+
+    let normalizedParticlesHistory = await Promise.all(particlesStep[0].map((particle, idx) => {
+        return new Promise(async (resolve, reject) => {
+            let hist = {mass: particle.mass, positions: []}
+            hist.positions = await Promise.all(particlesStep.map(step => {
+                return new Promise((resolve1, reject1) => {
+                    resolve1([(step[idx].position[0] - maxBBox.tl[0]) * ratio, SVG_SIZE - ((step[idx].position[1] - maxBBox.br[1]) * ratio)])
+                })
+            }))
+            resolve(hist)
+        })
+    }));
+    return normalizedParticlesHistory
+
+}
+
+async function printCircles(svgParticlesHist) {
+    await Promise.all(svgParticlesHist.map((pHist, idx) => {
+        return new Promise((resolve, reject) => {
+            appendCircle(pHist.positions[0][0], pHist.positions[0][1], "c_" + idx)
+            resolve()
+        })
+    }))
+
 }
 
 // given the history I set up an array of animations, with the keyframes of each circle
-function setUpCirclesAnimation() {
-    circlesAnimation = circlesHistory[0].map((circle) => {
-        let circleKeyFrames = [];
-        // I retrieve all the steps of the circle from the history
-        for (let i = 0; i < circlesHistory.length; i++) {
-            circleKeyFrames.push({cx: circlesHistory[i][circle.idx].cx, cy: circlesHistory[i][circle.idx].cy});
-        }
-        return anime({
-            targets: "svg#svg-canvas #" + circle.id,
-            // Properties
-            keyframes: circleKeyFrames,
-            duration: N_STEPS * 500, //each step will last 500ms
-            autoplay: false,
-            easing: 'linear',
-        });
-    });
+async function setUpCirclesAnimation(svgParticlesHist) {
+    let circlesAnimation = await Promise.all(svgParticlesHist.map((pHist, idx) => {
+        return new Promise(async (resolve, reject) => {
+            let circleKeyFrames = await Promise.all(pHist.positions.map(p => {
+                return new Promise((resolve1, reject1) => resolve1({cx: p[0], cy: p[1]}))
+            }));
+            resolve(anime({
+                targets: "svg#svg-canvas #" + "c_" + idx,
+                // Properties
+                keyframes: circleKeyFrames,
+                duration: N_STEPS * 125, //each step will last 500ms
+                autoplay: false,
+                easing: 'linear',
+            }));
+        })
+    }));
+    return circlesAnimation
 }
 
-function startCirclesAnimation() {
-    circlesAnimation.map((circleAnimation) => {
-        circleAnimation.play();
-    });
+async function startCirclesAnimation(circlesAnimation) {
+    await Promise.all(circlesAnimation.map((circleAnimation) => {
+        return new Promise((resolve, reject1) => {
+            circleAnimation.play();
+            resolve()
+        })
+    }))
+
 }
 
-function simulateParticlesAnimation() {
+async function simulateParticlesAnimation(svgParticlesHist) {
+    await printCircles(svgParticlesHist)
 
+    let circlesAnimation = await setUpCirclesAnimation(svgParticlesHist)
+
+    setTimeout(async () => {
+        await startCirclesAnimation(circlesAnimation)
+    }, 1)
+
+}
+
+async function initialSetup(obj) {
     document.getElementById("welcome-wrapper").setAttribute("display", "none");
     document.getElementById("welcome-wrapper").setAttribute("style", "display: none;");
     clearSvg();
-    circlesHistory = [];
-    circlesAnimation = [];
 
-    generateCircles();
+    console.log(obj);
 
-    appendCircles();
-    setUpCirclesAnimation();
-    startCirclesAnimation();
+    // get list of bBoxes
+    let bBoxes = await getBBoxes(obj['data'])
+    console.log(bBoxes)
+
+    // find max bBox
+    let maxBBox = await getMaxBBox(bBoxes)
+    console.log("max bBox " + maxBBox.tl + " - " + maxBBox.br)
+
+    //extract particles steps
+    let particlesStep = await getParticlesSteps(obj['data'])
+    console.log(particlesStep)
+
+    //make normalized particle history
+    let normalizedParticlesHistory = await getSvgNormalizedHistory(maxBBox, particlesStep)
+    console.log(normalizedParticlesHistory)
+
+    //simulateParticlesAnimation(normalizedParticlesHistory, maxBBox.br[0] - maxBBox.tl[0]);
+    return normalizedParticlesHistory
+
+}
+
+async function readFile(node) {
+    let obj = await fileToJSON(node.files[0])
+    hist = await initialSetup(obj)
+    setTimeout(async () => {
+        await simulateParticlesAnimation(hist)
+    }, 1)
 }
--- webgui/scripts/quadtree.js
+++ webgui/scripts/quadtree.js
@@ -1,10 +1,111 @@
 console.log("loaded quadtree.js");
 
+var currentStep = 1
+var totalSteps = null
+var globalQuadtreeStepsData = null
+var maxBBox = null
+
+async function getBBoxes(data) {
+    return await Promise.all(data.map(async (o) => {
+        return new Promise((resolve, reject) => {
+            resolve({
+                tl: o.quadtree.top_left,
+                br: [o.quadtree.top_left[0] + o.quadtree['length'], o.quadtree.top_left[1] - o.quadtree['length']]
+            })
+        })
+    }));
+
+}
+
+function getMaxBBox(bBoxes) {
+    let maxBBox = {
+        br: [-Infinity, Infinity],
+        tl: [Infinity, -Infinity]
+    }
+
+    bBoxes.forEach(function (bBox) {
+        if (bBox.tl[0] < maxBBox.tl[0]) maxBBox.tl[0] = bBox.tl[0];
+        if (bBox.tl[1] > maxBBox.tl[1]) maxBBox.tl[1] = bBox.tl[1];
+        if (bBox.br[0] > maxBBox.br[0]) maxBBox.br[0] = bBox.br[0];
+        if (bBox.br[1] < maxBBox.br[1]) maxBBox.br[1] = bBox.br[1];
+    })
+
+    return maxBBox
+}
+
+function normalizeQuadtree(idx, maxBBox) {
+    let data = JSON.parse(JSON.stringify(globalQuadtreeStepsData))
+
+    function navigateQuadtree(quadrant) {
+        console.log("tl : " + quadrant.top_left)
+        quadrant.top_left[0] = quadrant.top_left[0] - maxBBox.tl[0]
+        quadrant.top_left[1] = quadrant.top_left[1] - maxBBox.br[1]
+        console.log("normal tl : " + quadrant.top_left)
+        if (quadrant.data === "empty") {
+            return;
+        }
+        quadrant.center_of_mass[0] = quadrant.center_of_mass[0] - maxBBox.tl[0]
+        quadrant.center_of_mass[1] = quadrant.center_of_mass[1] - maxBBox.br[1]
+        if ("body" in quadrant.data) {
+            quadrant.data.body.position[0] = quadrant.data.body.position[0] - maxBBox.tl[0]
+            quadrant.data.body.position[1] = quadrant.data.body.position[1] - maxBBox.br[1]
+        }
+
+        if ("subquadrants" in quadrant.data) {
+            navigateQuadtree(quadrant.data.subquadrants.nw);
+            navigateQuadtree(quadrant.data.subquadrants.ne);
+            navigateQuadtree(quadrant.data.subquadrants.se);
+            navigateQuadtree(quadrant.data.subquadrants.sw);
+        }
+
+    }
+
+    console.log(data[idx])
+    navigateQuadtree(data[idx])
+
+    return data[idx]
+}
+
 async function readFile(node) {
     const file = node.files[0];
     const obj = await fileToJSON(file);
-    console.log(obj);
-    drawQuadtree(obj);
+    globalQuadtreeStepsData = obj['data'].map((s) => (s.quadtree))
+    console.log("INITIAL GLOBAL DATA", globalQuadtreeStepsData)
+    totalSteps = globalQuadtreeStepsData.length;
+
+    // get list of bBoxes
+    let bBoxes = await getBBoxes(obj['data'])
+
+    // find max bBox
+    maxBBox = getMaxBBox(bBoxes)
+    console.log("max bBox " + maxBBox.tl + " - " + maxBBox.br)
+
+    let q = normalizeQuadtree(currentStep, maxBBox)
+
+    console.log("after normalization = (" + currentStep + "/" + totalSteps + ") ", q)
+
+    console.log(globalQuadtreeStepsData)
+
+    drawQuadtree(q)
+    //drawQuadtree(obj);
+
+}
+
+function nextStep() {
+    if (currentStep < (totalSteps - 1)) {
+        currentStep++;
+        console.log("rendering step " + currentStep)
+        let q = normalizeQuadtree(currentStep, maxBBox)
+        drawQuadtree(q)
+    }
+}
+
+function prevStep() {
+    if (currentStep > 1) {
+        currentStep--;
+        let q = normalizeQuadtree(currentStep, maxBBox)
+        drawQuadtree(q)
+    }
 }
 
 function drawQuadtree(quadtree) {
