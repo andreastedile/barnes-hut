204ffc99b473ee0b7d0c444c94930656b59cb87f
andreastedile <andrea.stedile@studenti.unitn.it>
2022-06-22 16:53:40 +0200

    Parse arguments with argparse library
    ./app bodies-250.txt 1000 0.5 -G 1 -omega 0.5

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,16 +1,46 @@
+#include <argparse/argparse.hpp>
 #include <iostream>
 #include <string>
 
+#include "force.h"
 #include "local_barnes_hut_simulator.h"
 
-int main(int, char* argv[]) {
-  // Todo: refactor using an argument parser
+int main(int argc, char* argv[]) {
+  argparse::ArgumentParser app("Barnes–Hut simulation");
+  app.add_argument("input")
+      .required()
+      .help("specify the input file");
+  app.add_argument("steps")
+      .scan<'d', int>()
+      .required()
+      .help("specify the number of simulation steps");
+  app.add_argument("dt")
+      .scan<'g', double>()
+      .required()
+      .help("specify the simulation gravitational constant");
+  app.add_argument("-G")
+      .scan<'g', double>()
+      .default_value(bh::NEWTONIAN_G)
+      .help("specify the simulation dt");
+  app.add_argument("-omega")
+      .scan<'g', double>()
+      .default_value(bh::DEFAULT_OMEGA)
+      .help("specify the simulation omega");
+  try {
+    app.parse_args(argc, argv);
+  } catch (const std::runtime_error& err) {
+    // std::cerr << err.what() << '\n';
+    std::cerr << app;
+    std::exit(1);
+  }
+
   bh::LocalBarnesHutSimulator simulator(
-      argv[1], // bodies.txt file
-      std::stod(argv[3]), // dt
-      std::stod(argv[4]), // G
-      std::stod(argv[5])); // omega
-  simulator.step_continuously(std::stoi(argv[2]));
+      app.get<std::string>("input"),
+      app.get<double>("dt"),
+      app.get<double>("-G"),
+      app.get<double>("-omega"));
+  simulator.step_continuously(app.get<int>("steps"));
+
   std::cout << "Simulation completed. Saving JSON...\n";
   simulator.save_json();
   return 0;
