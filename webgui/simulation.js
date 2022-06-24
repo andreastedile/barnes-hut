2e48d8ef93547b22b21f0aa0cab451acb96e8d2a
andreastedile <andrea.stedile@studenti.unitn.it>
2022-06-25 00:05:35 +0200

    Improve variables naming

--- webgui/simulation.js
+++ webgui/simulation.js
@@ -11,51 +11,46 @@ function quadtreeToSvg(quadtree) {
     const svg = d3.create("svg");
 
     // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/viewBox
-    const bbox = quadtree.boundingBox;
-    const bl = bbox.bottomLeft;
-    const tr = bbox.topRight;
-    const minX = bl.x;
-    const minY = bl.y;
-    const maxX = tr.x;
-    const maxY = tr.y;
-    const bboxWidth = Math.abs(maxX - minX);
-    const bboxHeight = Math.abs(maxY - minY);
-    svg.attr("viewBox", `${minX} ${minY} ${bboxWidth} ${bboxHeight}`)
-        .attr("width", window.innerHeight)
+    const rootBbox = quadtree.boundingBox;
+    const rootMinX = rootBbox.bottomLeft.x;
+    const rootMinY = rootBbox.bottomLeft.y;
+    const rootMaxX = rootBbox.topRight.x;
+    const rootMaxY = rootBbox.topRight.y;
+    const rootWidth = Math.abs(rootMaxX - rootMinX);
+    const rootHeight = Math.abs(rootMaxY - rootMinY);
+    svg.attr("viewBox", `${rootMinX} ${rootMinY} ${rootWidth} ${rootHeight}`)
+        .attr("width", window.innerWidth)
         .attr("height", window.innerHeight);
 
-    const quadtrees = [quadtree];
-    while (quadtrees.length > 0) {
-        const qt = quadtrees.shift();
+    const nodes_to_process = [quadtree];
+    while (nodes_to_process.length > 0) {
+        const node = nodes_to_process.shift();
 
-        const rectBbox = qt.boundingBox;
-        const rectBl = rectBbox.bottomLeft;
-        const rectTl = rectBbox.topRight;
-        const rectMinX = rectBl.x;
-        const rectMinY = rectBl.y;
-        const rectMaxX = rectTl.x;
-        const rectMaxY = rectTl.y;
-        const rectWidth = Math.abs(rectMaxX - rectMinX);
-        const rectHeight = Math.abs(rectMaxY - rectMinY);
+        const nodeBbox = node.boundingBox;
+        const nodeMinX = nodeBbox.bottomLeft.x;
+        const nodeMinY = nodeBbox.bottomLeft.y;
+        const nodeMaxX = nodeBbox.topRight.x;
+        const nodeMaxY = nodeBbox.topRight.y;
+        const nodeWidth = Math.abs(nodeMaxX - nodeMinX);
+        const nodeHeight = Math.abs(nodeMaxY - nodeMinY);
 
-        const STROKE_WIDTH = 0.5;
+        const LINE_THICKNESS = 0.5;
         svg.append("rect")
-            .attr("x", rectMinX)
-            .attr("y", rectMinY)
-            .attr("width", rectWidth)
-            .attr("height", rectHeight)
-            .style("stroke-width", STROKE_WIDTH)
+            .attr("x", nodeMinX)
+            .attr("y", nodeMinY)
+            .attr("width", nodeWidth)
+            .attr("height", nodeHeight)
+            .style("stroke-width", LINE_THICKNESS)
             .style("stroke", "grey")
             .style("opacity", "0.5")
             .style("fill", "none")
             .style("vector-effect", "non-scaling-stroke")
 
-        if (qt.fork) {
-            quadtrees.push(qt.fork.nw, qt.fork.ne, qt.fork.se, qt.fork.sw);
-        } else if (qt.leaf.body) {
-            body = qt.leaf.body;
-            let CIRCLE_RADIUS = bboxWidth / 100;
-            CIRCLE_RADIUS *= body.mass;
+        if (node.fork) {
+            nodes_to_process.push(node.fork.nw, node.fork.ne, node.fork.se, node.fork.sw);
+        } else if (node.leaf.body) {
+            body = node.leaf.body;
+            const CIRCLE_RADIUS = (rootWidth / 100) * body.mass;
             svg.append("circle")
                 .style("vector-effect", "non-scaling-stroke")
                 .attr("cx", body.position.x)
