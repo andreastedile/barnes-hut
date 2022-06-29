4483f0467566dd3bc642b9a5881530278a5afba4
andreastedile <andrea.stedile@studenti.unitn.it>
2022-06-29 14:15:29 +0200

    minor

--- src/simulation/mpi_barnes_hut_simulator.cpp
+++ src/simulation/mpi_barnes_hut_simulator.cpp
@@ -9,11 +9,11 @@ AlignedBox2d compute_bounding_box_for_processor(const AlignedBox2d& bbox, int n_
   const int N_ROWS = static_cast<int>(std::sqrt(n_procs));
   const int N_COLS = N_ROWS;
 
-  double min_x = bbox.min().x() + (proc_id % N_ROWS) * (bbox.sizes().x() / N_ROWS);
-  double max_x = min_x + (bbox.sizes().x() / N_ROWS);
+  double min_x = bbox.min().x() + (proc_id % N_COLS) * (bbox.sizes().x() / N_COLS);
+  double max_x = min_x + (bbox.sizes().x() / N_COLS);
 
-  double min_y = bbox.min().y() + std::floor(proc_id / N_COLS) * (bbox.sizes().y() / N_COLS);
-  double max_y = min_y + (bbox.sizes().y() / N_COLS);
+  double min_y = bbox.min().y() + std::floor(proc_id / N_ROWS) * (bbox.sizes().y() / N_ROWS);
+  double max_y = min_y + (bbox.sizes().y() / N_ROWS);
 
   return {Vector2d{min_x, min_y}, Vector2d{max_x, max_y}};
 }
