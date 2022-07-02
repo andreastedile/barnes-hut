be4bcd620e12253120356a48717b286a3f9be88b
andreastedile <andrea.stedile@studenti.unitn.it>
2022-07-02 17:26:27 +0200

    Minor update to doc

--- src/barnes_hut/bounding_box.h
+++ src/barnes_hut/bounding_box.h
@@ -12,8 +12,7 @@ namespace bh {
 
 /**
  * Computes the axis-aligned minimum bounding box containing some bodies.
- * @details
- * The minimum bounding box is defined by:
+ * @details The minimum bounding box is defined by:
  * <ul>
  * <li> Its bottom-left corner, whose x and y coordinates are minimal for any of the bodies
  * <li> Its top-right corner, whose x and y coordinates are maximal for any of the bodies
@@ -33,14 +32,10 @@ AlignedBox2d compute_minimum_bounding_box(const std::vector<Body> &bodies);
 
 /**
  * Computes an axis-aligned square bounding box containing some bodies.
- * @details The bounding box is defined by its bottom-left and top-right
- * corners. The computed bounding box is not minimum, but is
- *
- * @param bodies for which to compute the minimum bounding box; must be at least
- * two
- * @return the minimum bounding box; x() and y() return its bottom-left and
- * top-right corners
- * @throw invalid_argument if the bodies vector contains less than two bodies
+ * @details The bounding box is defined by its bottom-left and top-right corners.
+ * Todo
+ * @param bodies for which to compute the minimum bounding box; can be empty
+ * @return the square bounding box; x() and y() return its bottom-left and top-right corners
  */
 AlignedBox2d compute_square_bounding_box(const std::vector<Body> &bodies);
 
