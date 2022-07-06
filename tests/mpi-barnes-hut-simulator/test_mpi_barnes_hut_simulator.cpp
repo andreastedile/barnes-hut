6b333653e34d133ba8d9d48e5c7e50e118dfc542
andreastedile <andrea.stedile@studenti.unitn.it>
2022-07-06 15:47:59 +0200

    test filter bodies by subquadrant

--- tests/mpi-barnes-hut-simulator/test_mpi_barnes_hut_simulator.cpp
+++ tests/mpi-barnes-hut-simulator/test_mpi_barnes_hut_simulator.cpp
@@ -113,3 +113,51 @@ TEST_CASE("compute bounding box for 4 processors; negative coordinates only") {
   REQUIRE(q3.max() == Eigen::Vector2d{-2, -2});
 }
 
+// https://www.desmos.com/calculator/3b3drqark3?lang=it
+TEST_CASE("filter bodies by subquadrant") {
+  auto p1 = Eigen::Vector2d{0, 0};
+  auto p2 = Eigen::Vector2d{5, 0};
+  auto p3 = Eigen::Vector2d{10, 0};
+  auto p4 = Eigen::Vector2d{0, 5};
+  auto p5 = Eigen::Vector2d{5, 5};
+  auto p6 = Eigen::Vector2d{10, 5};
+  auto p7 = Eigen::Vector2d{0, 10};
+  auto p8 = Eigen::Vector2d{5, 10};
+  auto p9 = Eigen::Vector2d{10, 10};
+
+  std::vector<bh::Body> bodies{bh::Body{p1, 0.25}, bh::Body{p2, 0.25}, bh::Body{p3, 0.25},
+                               bh::Body{p4, 0.25}, bh::Body{p5, 0.25}, bh::Body{p6, 0.25},
+                               bh::Body{p7, 0.25}, bh::Body{p8, 0.25}, bh::Body{p9, 0.25}};
+
+  auto bbox1 = Eigen::AlignedBox2d{Eigen::Vector2d{0, 0}, Eigen::Vector2d{5, 5}};
+  auto bbox2 = Eigen::AlignedBox2d{Eigen::Vector2d{5, 0}, Eigen::Vector2d{10, 5}};
+  auto bbox3 = Eigen::AlignedBox2d{Eigen::Vector2d{0, 5}, Eigen::Vector2d{5, 10}};
+  auto bbox4 = Eigen::AlignedBox2d{Eigen::Vector2d{5, 5}, Eigen::Vector2d{10, 10}};
+
+  auto outer_bbox = Eigen::AlignedBox2d{Eigen::Vector2d{0, 0}, Eigen::Vector2d{10, 10}};
+
+  // blue bodies in the Desmos graph
+  auto bodies_inside_bbox1 = bh::filter_bodies_by_subquadrant(bodies, outer_bbox, bbox1);
+
+  // green bodies in the Desmos graph
+  auto bodies_inside_bbox2 = bh::filter_bodies_by_subquadrant(bodies, outer_bbox, bbox2);
+
+  // orange bodies in the Desmos graph
+  auto bodies_inside_bbox3 = bh::filter_bodies_by_subquadrant(bodies, outer_bbox, bbox3);
+
+  // red bodies in the Desmos graph
+  auto bodies_inside_bbox4 = bh::filter_bodies_by_subquadrant(bodies, outer_bbox, bbox4);
+
+  REQUIRE(bodies_inside_bbox1[0].m_position == p1);
+
+  REQUIRE(bodies_inside_bbox2[0].m_position == p2);
+  REQUIRE(bodies_inside_bbox2[1].m_position == p3);
+
+  REQUIRE(bodies_inside_bbox3[0].m_position == p4);
+  REQUIRE(bodies_inside_bbox3[1].m_position == p7);
+
+  REQUIRE(bodies_inside_bbox4[0].m_position == p5);
+  REQUIRE(bodies_inside_bbox4[1].m_position == p6);
+  REQUIRE(bodies_inside_bbox4[2].m_position == p8);
+  REQUIRE(bodies_inside_bbox4[3].m_position == p9);
+}
