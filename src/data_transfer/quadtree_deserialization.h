824e3dbc6fb74270c68130b966064ee5dfff1903
andreastedile <andrea.stedile@studenti.unitn.it>
2022-07-06 12:53:12 +0200

    Make function available in header

--- src/data_transfer/quadtree_deserialization.h
+++ src/data_transfer/quadtree_deserialization.h
@@ -13,6 +13,8 @@ using QuadtreeGrid = std::vector<std::vector<std::unique_ptr<Node>>>;
 
 QuadtreeGrid deserialize_quadtrees(int n_procs, const std::vector<mpi::Node> &quadtrees, const std::vector<int> &n_nodes);
 
+std::unique_ptr<Node> deserialize_quadtree(const std::vector<mpi::Node> &nodes);
+
 }  // namespace bh
 
 #endif  // BARNES_HUT_QUADTREE_DESERIALIZATION_H
