416b40ffad24ddc382122dd08d1352f7bafa8a69
andreastedile <andrea.stedile@studenti.unitn.it>
2022-07-06 09:43:41 +0200

    It is not possible anymore to construct a body with no mass

--- src/body/body.cpp
+++ src/body/body.cpp
@@ -21,7 +21,7 @@ std::istream &operator>>(std::istream &stream, Body &body) {
   stream >> body.m_position.x();
   stream >> body.m_position.y();
   stream >> body.m_mass;
-  if (body.m_mass < 0) {
+  if (body.m_mass <= 0) {
     throw std::runtime_error("invalid body mass (read " + std::to_string(body.m_mass) + ", should be non-negative)");
   }
   stream >> body.m_velocity.x();
