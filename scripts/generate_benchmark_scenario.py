f73634e8b0371ba5ae30b1a545ae9af971e0d4bd
andreastedile <andrea.stedile@studenti.unitn.it>
2022-07-10 22:08:24 +0200

    Refactored script

--- scripts/generate_benchmark_scenario.py
+++ scripts/generate_benchmark_scenario.py
@@ -7,60 +7,78 @@ from collections import namedtuple
 
 Position = namedtuple("Position", ["x", "y"])
 Velocity = namedtuple("Velocity", ["x", "y"])
-Body = namedtuple("Body", ["position", "mass", "velocity"])
+Planet = namedtuple("Planet", ["position", "mass", "velocity"])
 
-min_mass = 13000000000
-max_mass = 1898000000000000
+# Reference planetary system variables
+MIN_PLANET_MASS = 13000000000
+MAX_PLANET_MASS = 1898000000000000
+N_PLANETS_IN_SOLAR_SYSTEM = 10
+SOLAR_SYSTEM_SURFACE = 223267590144
 
-def parse_scenario_parameters() -> (int):
+
+def parse_scenario_parameters() -> int:
     """
-    Parses the command-line arguments
+    Parses the command-line arguments to obtain the number of planets to be added to the simulation scenario
+    :return: number of planets
     """
-    parser = argparse.ArgumentParser(description="Generate a benchmarking dataset containing n bodies")
+    parser = argparse.ArgumentParser(description="Generate a benchmarking dataset containing n planets")
 
-    parser.add_argument("-p", "--n-bodies", type=int, required=True,
-                        help="Number of bodies to add inside the bounding box", metavar="\b", dest="p")
+    parser.add_argument("-n", "--n-planets", type=int, required=True,
+                        help="Number of planets to add inside the bounding box", metavar="\b", dest="p")
 
     args = parser.parse_args()
 
-    if(args.p < 0):
-        parser.error("number of bodies must be positive")
+    if args.p < 0:
+        parser.error("number of planets must non-negative")
 
     return args.p
 
 
+def compute_scenario_surface(n_scenario_planets: int) -> float:
+    """
+    :return: the total surface where the planets are to be inserted
+    """
+    # N_PLANETS_IN_SOLAR_SYSTEM : SOLAR_SYSTEM_SURFACE = n_scenario_planets : scenario_surface
+    return SOLAR_SYSTEM_SURFACE * n_scenario_planets / N_PLANETS_IN_SOLAR_SYSTEM
 
-# reference: for 10 bodies we have bbox length of 945024m and surface of 893070360576m^2
-def generate_bodies(n_bodies: int) -> [
-    (float, float, float, float, float)]:
-    global min_mass, max_mass
 
-    scenario_surface = (223267590144 * n_bodies) / 10
+def generate_planet(pos_min: Position, pos_max: Position):
+    """
+    Randomly generates a planet.
+    The planet's mass is between MIN_PLANET_MASS and MAX_PLANET_MASS.
+    The planet's position coordinates are between 0 and bbox_length.
+    :param bbox_length length of the bounding box in
+    :return: a planet located in a bounding box of length bbox_length, with
+    """
+    position = Position(random.uniform(pos_min.x, pos_max.x),
+                        random.uniform(pos_min.y, pos_max.y))
+    mass = random.uniform(MIN_PLANET_MASS, MAX_PLANET_MASS)
+    velocity = Velocity(0, 0)
+    return Planet(position, mass, velocity)
+
+
+def generate_scenario_planets(n_scenario_planets: int) -> [Planet]:
+    scenario_surface = compute_scenario_surface(n_scenario_planets)
     bbox_length = math.sqrt(scenario_surface)
 
-    bodies = []
-    for _ in range(n_bodies):
-        position = Position(random.uniform(0, bbox_length),
-                            random.uniform(0, bbox_length))
-        mass = random.uniform(min_mass, max_mass)
-        velocity = Velocity(0, 0)
-
-        body = Body(position, mass, velocity)
-        bodies.append(body)
-    return bodies
+    planets = []
+    for _ in range(n_scenario_planets):
+        planet = generate_planet(Position(0, 0), Position(bbox_length, bbox_length))
+        planets.append(planet)
+    return planets
 
 
-def write_bodies_to_file(bodies: list[Body]):
-    with open(f"{len(bodies)}-bodies.txt", "w") as f:
-        f.write(f"{len(bodies)}\n")
-        for body in bodies:
-            f.write(f"{body.position.x} {body.position.y} {body.mass} {body.velocity.x} {body.velocity.y}\n")
+def write_planets_to_file(planets: [Planet]):
+    with open(f"planets-{len(planets)}.txt", "w") as f:
+        f.write(f"{len(planets)}\n")
+        for planet in planets:
+            f.write(f"{planet.position.x} {planet.position.y} {planet.mass} {planet.velocity.x} {planet.velocity.y}\n")
 
 
 def main():
-    n_bodies = parse_scenario_parameters()
-    bodies = generate_bodies(n_bodies)
-    write_bodies_to_file(bodies)
+    n_scenario_planets = parse_scenario_parameters()
+    planets = generate_scenario_planets(n_scenario_planets)
+    write_planets_to_file(planets)
 
 
 if __name__ == "__main__":
---
+++ scripts/reference_system.md
@@ -0,0 +1,19 @@
+From https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+
+Dp = Pluto's distance from Sun = 5906.4 × 10⁶ km = 5.9064 × 10¹² m 
+
+Mp = Pluto's mass = 0.0130 × 10²⁴ kg 
+
+Mj = Jupiter's mass = 1898 × 10²⁴ kg
+
+*We now obtain the parameters of our reference planetary system:* 
+
+L = 2 × Dp / 10¹² = 11.8128 m
+
+**System length** = L * 4 * 10⁴ = 472512
+
+**System surface** = 223267590144
+
+**Planet min mass** = Mp / 10¹² = 0.0130 × 10¹² = 13000000000
+
+**Planet nax mass** = Mj /  10¹² = 1898 × 10¹² = 1.898 × 10¹⁵ = 1898000000000000
